cf862432e6a5c5adccad5282d8663c98ce56f788
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-05-09 10:25:14 -0500

    Se crea dto log create

--- src/modules/log/dto/create-log.dto.ts
+++ src/modules/log/dto/create-log.dto.ts
@@ -1,1 +1,7 @@
-export class CreateLogDto {}
+export class CreateLogDto {
+    log_id_caso_validado_FK: number;
+    log_id_usuario_FK: number;
+    log_accion: string;
+    log_ip: string;
+    log_estado: boolean;
+}
