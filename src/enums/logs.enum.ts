f13e7b5633096426ce4066830bde546529f788d5
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-04 15:27:24 -0500

    Se corrige a asignado a analista

--- src/enums/logs.enum.ts
+++ src/enums/logs.enum.ts
@@ -2,7 +2,7 @@ export enum logReports {
     LOG_CREATION = 'Registró el caso',
     LOG_VALIDATION = 'Validó el caso',
     LOG_ANULATION = 'Anuló el caso',
-    LOG_ASSIGNMENT_ANALYST = 'Asignó a analista',
-    LOG_REASSIGNMENT_ANALYST = 'Reasignó a analista',
-    LOG_ASSIGNMENT_INVESTIGATOR = 'Asignó a investigador',
+    LOG_ASSIGNMENT_ANALYST = 'Asignado a analista',
+    LOG_REASSIGNMENT_ANALYST = 'Reasignado a analista',
+    LOG_ASSIGNMENT_INVESTIGATOR = 'Asignado a investigador',
 };
