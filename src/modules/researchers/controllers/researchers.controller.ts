5499c333dd5be5735bffe8398590d20bbbe8ad20
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-05 10:34:16 -0500

    se define un endpoint GET para filtrar los investigadores

--- src/modules/researchers/controllers/researchers.controller.ts
+++ src/modules/researchers/controllers/researchers.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ResearchersService } from '../services/researchers.service';
 import { FilterResearcherDto } from '../dto/filter-researcher.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('researchers')
 @Controller('researchers')
 export class ResearchersController {
   constructor(private readonly researchersService: ResearchersService) {}
 
+  @Get('filter')
+  async filterResearchers(
+    @Query('empImmediateBoss') empImmediateBoss?: string,
+    @Query('empPosition') empPosition?: string
+  ) {
+    const filter: Partial<FilterResearcherDto> = {
+      empImmediateBoss,
+      empPosition
+    };
 
+    return this.researchersService.filterResearchers(filter);
+  }
 }
