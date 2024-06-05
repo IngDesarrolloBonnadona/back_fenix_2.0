2d3bcc56f8de632626c601ffdeb46d9f34ff6808
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-05 09:55:10 -0500

    se crea metodo para filtrar investigadores

--- src/modules/researchers/services/researchers.service.ts
+++ src/modules/researchers/services/researchers.service.ts
@@ -8,4 +8,16 @@ export class ResearchersService {
   constructor(
     private readonly httpResearchersService: HttpResearchersService
   ) {}
+
+  async filterResearchers(resFilter: Partial<FilterResearcherDto>) {
+    const response = await this.httpResearchersService.getResearchersData();
+    const researchers: FilterResearcherDto[] = response.data.data;
+
+    return researchers.filter(research => {
+      return (
+        (!resFilter.empImmediateBoss || research.empImmediateBoss === resFilter.empImmediateBoss) &&
+        (!resFilter.empPosition || research.empPosition === resFilter.empPosition)
+      );
+    });
+  }
 }
