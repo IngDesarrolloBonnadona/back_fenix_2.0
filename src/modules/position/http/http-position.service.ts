f5280cd630c6ce17f52abac772bec7c2fd93984b
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-06 17:38:52 -0500

    se crea la peticion http position

---
+++ src/modules/position/http/http-position.service.ts
@@ -0,0 +1,30 @@
+import { HttpService as NestHttpService } from '@nestjs/axios';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+require('dotenv').config();
+
+@Injectable()
+export class HttpPositionService {
+  constructor(private readonly httpPositionService: NestHttpService) {}
+
+  async getPositionData() {
+    try {
+      const url = process.env.URL_POSITIONS;
+      const response = firstValueFrom(
+        this.httpPositionService.get(url, {
+          headers: {
+            'X-Authorization': process.env.X_AUTH_VALUE_POSITION,
+          },
+        }),
+      );
+      console.log(response)
+      return response;
+    } catch (error) {
+      throw new HttpException(
+        `Hubo un error al consultar en la base de datos: ${error}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
