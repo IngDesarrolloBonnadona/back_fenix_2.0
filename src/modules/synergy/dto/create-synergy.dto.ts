d4c3ba946509d682332dea2663c73a243b833b1c
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-08 10:54:22 -0500

    se crea el dto

--- src/modules/synergy/dto/create-synergy.dto.ts
+++ src/modules/synergy/dto/create-synergy.dto.ts
@@ -1,1 +1,7 @@
-export class CreateSynergyDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateSynergyDto {
+    @IsNotEmpty()
+    @IsString()
+    syn_validatedcase_id_fk: string;
+}
