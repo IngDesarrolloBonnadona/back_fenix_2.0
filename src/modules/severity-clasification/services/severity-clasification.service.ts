4377ec4167175bc0f7a7aedf60868d3c3590db93
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-15 10:13:09 -0500

    se agrega las relaciones en el metodo findOneSeverityClasification

--- src/modules/severity-clasification/services/severity-clasification.service.ts
+++ src/modules/severity-clasification/services/severity-clasification.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateSeverityClasificationDto } from '../dto/create-severity-clasification.dto';
 import { UpdateSeverityClasificationDto } from '../dto/update-severity-clasification.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,30 +17,39 @@ export class SeverityClasificationService {
     private readonly severityClasifRepository: Repository<SeverityClasifEntity>,
   ) {}
 
-  async createSeverityClasification(createSeverityClasificationDto: CreateSeverityClasificationDto) {
-    const severityClasif = this.severityClasifRepository.create(createSeverityClasificationDto);
+  async createSeverityClasification(
+    createSeverityClasificationDto: CreateSeverityClasificationDto,
+  ) {
+    const severityClasif = this.severityClasifRepository.create(
+      createSeverityClasificationDto,
+    );
     return await this.severityClasifRepository.save(severityClasif);
   }
 
   async findAllSeverityClasifications() {
     const severityClasifs = await this.severityClasifRepository.find({
       relations: {
-        caseReportOriginal: true
-      }
+        caseReportOriginal: true,
+      },
     });
 
-    if (!severityClasifs || severityClasifs.length === 0) {
+    if (severityClasifs.length === 0) {
       throw new HttpException(
         'No se encontró la lista de clasificaciones de severidad',
         HttpStatus.NO_CONTENT,
       );
     }
 
-    return severityClasifs
+    return severityClasifs;
   }
 
   async findOneSeverityClasification(id: number) {
-    const severityClasif = await this.severityClasifRepository.findOne({ where: { id } });
+    const severityClasif = await this.severityClasifRepository.findOne({
+      where: { id },
+      relations: {
+        caseReportOriginal: true,
+      },
+    });
 
     if (!severityClasif) {
       throw new HttpException(
@@ -44,19 +58,25 @@ export class SeverityClasificationService {
       );
     }
 
-    return severityClasif
+    return severityClasif;
   }
 
-  async updateSeverityClasification(id: number, updateSeverityClasificationDto: UpdateSeverityClasificationDto) {
+  async updateSeverityClasification(
+    id: number,
+    updateSeverityClasificationDto: UpdateSeverityClasificationDto,
+  ) {
     const severityClasif = await this.findOneSeverityClasification(id);
-    const result = await this.severityClasifRepository.update(severityClasif.id, updateSeverityClasificationDto);
-    
+    const result = await this.severityClasifRepository.update(
+      severityClasif.id,
+      updateSeverityClasificationDto,
+    );
+
     if (result.affected === 0) {
       return new HttpException(
         `No se pudo actualizar la clasificacion de severidad`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
-    }  
+    }
 
     return new HttpException(
       `¡Datos actualizados correctamente!`,
@@ -66,14 +86,16 @@ export class SeverityClasificationService {
 
   async deleteSeverityClasification(id: number) {
     const severityClasif = await this.findOneSeverityClasification(id);
-    const result = await this.severityClasifRepository.softDelete(severityClasif.id);
+    const result = await this.severityClasifRepository.softDelete(
+      severityClasif.id,
+    );
 
     if (result.affected === 0) {
       return new HttpException(
         `No se pudo eliminar la clasificacion de severidad.`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
-    }  
+    }
     return new HttpException(
       `¡Datos eliminados correctamente!`,
       HttpStatus.ACCEPTED,
