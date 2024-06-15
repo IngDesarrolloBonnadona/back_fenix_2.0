1ec52bdd5fc1523ba6cfef373b25e8102c054f69
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-15 10:11:46 -0500

    se agrega las relaciones en el metodo findOneService

--- src/modules/service/services/service.service.ts
+++ src/modules/service/services/service.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateServiceDto } from '../dto/create-service.dto';
 import { UpdateServiceDto } from '../dto/update-service.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,9 +13,9 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ServiceService {
   constructor(
-  @InjectRepository(ServiceEntity)
-    private readonly serviceRepository: Repository<ServiceEntity>
-  ){}
+    @InjectRepository(ServiceEntity)
+    private readonly serviceRepository: Repository<ServiceEntity>,
+  ) {}
 
   async createService(createServiceDto: CreateServiceDto) {
     const service = this.serviceRepository.create(createServiceDto);
@@ -21,21 +26,27 @@ export class ServiceService {
     const services = await this.serviceRepository.find({
       relations: {
         unit: true,
-        caseReportOriginal: true
-      }
+        caseReportOriginal: true,
+      },
     });
 
-    if (!services || services.length === 0) {
+    if (services.length === 0) {
       throw new HttpException(
         'No se encontró la lista de servicios',
         HttpStatus.NO_CONTENT,
       );
     }
-    return services
+    return services;
   }
 
   async findOneService(id: number) {
-    const service = await this.serviceRepository.findOne({ where: { id } });
+    const service = await this.serviceRepository.findOne({
+      where: { id },
+      relations: {
+        unit: true,
+        caseReportOriginal: true,
+      },
+    });
 
     if (!service) {
       throw new HttpException(
@@ -43,19 +54,22 @@ export class ServiceService {
         HttpStatus.NO_CONTENT,
       );
     }
-    return service
+    return service;
   }
 
   async updateService(id: number, updateServiceDto: UpdateServiceDto) {
     const service = await this.findOneService(id);
-    const result = await this.serviceRepository.update(service.id, updateServiceDto);
-    
+    const result = await this.serviceRepository.update(
+      service.id,
+      updateServiceDto,
+    );
+
     if (result.affected === 0) {
       return new HttpException(
         `No se pudo actualizar el servicio`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
-    }  
+    }
 
     return new HttpException(
       `¡Datos actualizados correctamente!`,
@@ -72,7 +86,7 @@ export class ServiceService {
         `No se pudo eliminar el servicio.`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
-    }  
+    }
     return new HttpException(
       `¡Datos eliminados correctamente!`,
       HttpStatus.ACCEPTED,
