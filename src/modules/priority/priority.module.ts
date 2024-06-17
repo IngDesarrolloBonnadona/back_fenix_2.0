c339f8aa00ef2834cb13257148a1043a6a5a07ce
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-17 18:03:11 -0500

    se crea los metodos del servicio

--- src/modules/priority/priority.module.ts
+++ src/modules/priority/priority.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PriorityService } from './services/priority.service';
 import { PriorityController } from './controllers/priority.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Priority } from './entities/priority.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Priority])
+  ],
   controllers: [PriorityController],
   providers: [PriorityService],
 })
