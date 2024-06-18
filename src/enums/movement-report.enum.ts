58696d995e1fa6fb9c4767438fd28d698c57b65c
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-18 14:44:03 -0500

    se agrega nuevos enum en movementReport

--- src/enums/movement-report.enum.ts
+++ src/enums/movement-report.enum.ts
@@ -1,6 +1,12 @@
 export enum movementReport {
-    REPORT_CREATION = 'Creación de reporte',
-    VALIDATION = 'Validación',
-    REACTIVATION = 'Reactivación',
-    ANULATION = 'Anulación'
+    REPORT_CREATION = 'Reportado',
+    VALIDATION = 'Validado',
+    REACTIVATION = 'Reactivado',
+    ANULATION = 'Anulado',
+    ASSIGNMENT_ANALYST = 'Asignado a analista',
+    REASSIGNMENT_ANALYST = 'Reasignado a analista',
+    ASSIGNMENT_INVESTIGATOR = 'Asignado a investigador',
+    CASE_RAISED_SYNERGY_COMMITTEE = 'Caso elevado a comité de sinergia',
+    CASE_RESCHEDULED_SYNERGY = 'Reprogramó el caso en sinergia',
+    SOLUTION_CASE_SYNERGY = 'Solucionó el caso en sinergia'
 };
