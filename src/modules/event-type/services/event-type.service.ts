6993d13697a8f0b9f1c891e6e354246f59ce9e0d
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-24 14:51:23 -0500

    se agrega validacion de la existencia antes de agregar registro nuevo

--- src/modules/event-type/services/event-type.service.ts
+++ src/modules/event-type/services/event-type.service.ts
@@ -20,6 +20,20 @@ export class EventTypeService {
   async createEventType(
     createEventTypeDto: CreateEventTypeDto,
   ): Promise<EventTypeEntity> {
+    const eventTypes = await this.eventTypeRepository.findOne({
+      where: {
+        eve_t_name: createEventTypeDto.eve_t_name,
+        eve_t_casetype_id_FK: createEventTypeDto.eve_t_casetype_id_FK
+      }
+    });
+
+    if (eventTypes) {
+      throw new HttpException(
+        'El tipo de suceso ya existe con el tipo de caso.',
+        HttpStatus.NO_CONTENT,
+      );
+    }
+
     const eventType = this.eventTypeRepository.create(createEventTypeDto);
     return await this.eventTypeRepository.save(eventType);
   }
