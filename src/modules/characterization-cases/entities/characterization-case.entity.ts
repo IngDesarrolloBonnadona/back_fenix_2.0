98f68268cd8f3025ff78e50a90f1748e0387789a
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-25 16:23:21 -0500

    se crea la entidad

--- src/modules/characterization-cases/entities/characterization-case.entity.ts
+++ src/modules/characterization-cases/entities/characterization-case.entity.ts
@@ -1,1 +1,25 @@
-export class CharacterizationCase {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class CharacterizationCase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  cha_c_name: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  cha_c_description: string;
+
+  @Column({ type: 'boolean', default: true })
+  cha_c_status: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
