0e04b33ee43ab7fd1a404d70aec10a0ecfe0b858
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-06-25 16:26:23 -0500

    se asigna los atributos al DTO

--- src/modules/characterization-cases/dto/create-characterization-case.dto.ts
+++ src/modules/characterization-cases/dto/create-characterization-case.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCharacterizationCaseDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateCharacterizationCaseDto {
+    @IsNotEmpty()
+    @IsString()
+    cha_c_name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    cha_c_description: string;
+}
