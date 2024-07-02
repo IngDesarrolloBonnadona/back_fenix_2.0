b1cf69fb47f693ce9e7e9ce6404392802ca8fd4b
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-02 09:33:57 -0500

    se crea Crud

--- src/modules/observation-return-case/controllers/observation-return-case.controller.ts
+++ src/modules/observation-return-case/controllers/observation-return-case.controller.ts
@@ -10,45 +10,51 @@ import {
 import { ObservationReturnCaseService } from '../services/observation-return-case.service';
 import { CreateObservationReturnCaseDto } from '../dto/create-observation-return-case.dto';
 import { UpdateObservationReturnCaseDto } from '../dto/update-observation-return-case.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('observation-return-case')
 @Controller('observation-return-case')
 export class ObservationReturnCaseController {
   constructor(
     private readonly observationReturnCaseService: ObservationReturnCaseService,
   ) {}
 
-  @Post()
-  create(
+  @Post('/createObservationReturnCase/:idUser/:idCaseValidate')
+  createObservationReturnCase(
+    @Param('idUser') idUser: number,
+    @Param('idCaseValidate') idCaseValidate: string,
     @Body() createObservationReturnCaseDto: CreateObservationReturnCaseDto,
   ) {
     return this.observationReturnCaseService.create(
       createObservationReturnCaseDto,
+      idUser,
+      idCaseValidate
     );
   }
 
-  @Get()
-  findAll() {
-    return this.observationReturnCaseService.findAll();
+  @Get('/listObservationReturnCases')
+  listObservationReturnCases() {
+    return this.observationReturnCaseService.findAllObservationReturnCase();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.observationReturnCaseService.findOne(+id);
+  @Get('/findObservationReturnCase/:id')
+  findObservationReturnCase(@Param('id') id: number) {
+    return this.observationReturnCaseService.findOneObservationReturnCase(id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
+  @Patch('/updateObservationReturnCase/:id')
+  updateObservationReturnCase(
+    @Param('id') id: number,
     @Body() updateObservationReturnCaseDto: UpdateObservationReturnCaseDto,
   ) {
-    return this.observationReturnCaseService.update(
-      +id,
+    return this.observationReturnCaseService.updateObservationReturnCase(
+      id,
       updateObservationReturnCaseDto,
     );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.observationReturnCaseService.remove(+id);
+  @Delete('deleteObservationReturnCase:id')
+  deleteObservationReturnCase(@Param('id') id: number) {
+    return this.observationReturnCaseService.deleteObservationReturnCase(id);
   }
 }
--- src/modules/observation-return-case/services/observation-return-case.service.ts
+++ src/modules/observation-return-case/services/observation-return-case.service.ts
@@ -1,29 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, forwardRef } from '@nestjs/common';
 import { CreateObservationReturnCaseDto } from '../dto/create-observation-return-case.dto';
 import { UpdateObservationReturnCaseDto } from '../dto/update-observation-return-case.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ObservationReturnCase as ObservationReturnCaseEntity } from '../entities/observation-return-case.entity';
+import { Repository } from 'typeorm';
+import { ReasonReturnCaseService } from 'src/modules/reason-return-case/services/reason-return-case.service';
+import { CaseReportValidateService } from 'src/modules/case-report-validate/services/case-report-validate.service';
 
 @Injectable()
 export class ObservationReturnCaseService {
-  create(createObservationReturnCaseDto: CreateObservationReturnCaseDto) {
-    return 'This action adds a new observationReturnCase';
+  constructor(
+    @InjectRepository(ObservationReturnCaseEntity)
+    private readonly observationReturnRepository: Repository<ObservationReturnCaseEntity>,
+
+    private readonly reasonReturnCaseService: ReasonReturnCaseService,
+    @Inject(forwardRef(() => CaseReportValidateService))
+    private readonly caseReportValidateService: CaseReportValidateService,
+  ) {}
+
+  async create(
+    createObservationReturnCaseDto: CreateObservationReturnCaseDto,
+    idUser: number,
+    idCaseValidate: string,
+  ) {
+    await this.caseReportValidateService.findOneReportValidate(idCaseValidate);
+    await this.reasonReturnCaseService.findOneReasonReturnCase(
+      createObservationReturnCaseDto.rec_o_reasonreturn_id_fk,
+    );
+
+    const observationReturns = this.observationReturnRepository.create({
+      ...createObservationReturnCaseDto,
+      rec_o_user_id: idUser,
+      rec_o_validatedcase_id_fk: idCaseValidate,
+    });
+    return await this.observationReturnRepository.save(observationReturns);
   }
 
-  findAll() {
-    return `This action returns all observationReturnCase`;
+  async findAllObservationReturnCase(): Promise<ObservationReturnCaseEntity[]> {
+    const observationReturns = await this.observationReturnRepository.find({
+      where: {
+        rec_o_status: true,
+      },
+    });
+
+    if (observationReturns.length === 0) {
+      throw new HttpException(
+        `No se encontró la lista de observaciones de devolución del caso`,
+        HttpStatus.NO_CONTENT,
+      );
+    }
+    return observationReturns;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} observationReturnCase`;
+  async findOneObservationReturnCase(
+    id: number,
+  ): Promise<ObservationReturnCaseEntity> {
+    const observationReturn = await this.observationReturnRepository.findOne({
+      where: {
+        id,
+        rec_o_status: true,
+      },
+    });
+
+    if (!observationReturn) {
+      throw new HttpException(
+        'No se encontró la observación de devolución del caso',
+        HttpStatus.NO_CONTENT,
+      );
+    }
+    return observationReturn;
   }
 
-  update(
+  async updateObservationReturnCase(
     id: number,
     updateObservationReturnCaseDto: UpdateObservationReturnCaseDto,
   ) {
-    return `This action updates a #${id} observationReturnCase`;
+    const observationReturn = await this.findOneObservationReturnCase(id);
+    const result = await this.observationReturnRepository.update(
+      observationReturn.id,
+      updateObservationReturnCaseDto,
+    );
+
+    if (result.affected === 0) {
+      return new HttpException(
+        `No se pudo actualizar la observación de devolución del caso`,
+        HttpStatus.ACCEPTED,
+      );
+    }
+    return observationReturn;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} observationReturnCase`;
+  async deleteObservationReturnCase(id: number) {
+    const observationReturn = await this.findOneObservationReturnCase(id);
+    const result = await this.observationReturnRepository.softDelete(
+      observationReturn.id,
+    );
+
+    if (result.affected === 0) {
+      return new HttpException(
+        `No se pudo eliminar la observación de devolución del caso`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    return new HttpException(
+      `¡Datos eliminados correctamente!`,
+      HttpStatus.ACCEPTED,
+    );
   }
 }
