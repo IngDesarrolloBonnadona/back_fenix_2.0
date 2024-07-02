80f526823144123471fd9f6daba0f026cd83dfc8
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-02 09:33:53 -0500

    Se agrega rec_o_status como opcional

--- src/modules/observation-return-case/dto/update-observation-return-case.dto.ts
+++ src/modules/observation-return-case/dto/update-observation-return-case.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateObservationReturnCaseDto } from './create-observation-return-case.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateObservationReturnCaseDto extends PartialType(CreateObservationReturnCaseDto) {}
+export class UpdateObservationReturnCaseDto extends PartialType(CreateObservationReturnCaseDto) {
+    @IsOptional()
+    @IsBoolean()
+    rec_o_status?: boolean
+}
