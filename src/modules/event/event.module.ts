6b2154a3425d532a3b595189e89ada7f740df811
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-16 10:51:39 -0500

    se agrega los permisos al modulo event

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -4,13 +4,13 @@ import { EventController } from './controllers/event.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { EventTypeModule } from '../event-type/event-type.module';
+import { PermissionGuard } from 'src/guards/permission.guard';
+import { UserModule } from 'src/modules_bonnadonahub/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event]),
-  EventTypeModule
-],
+  imports: [TypeOrmModule.forFeature([Event]), EventTypeModule, UserModule],
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, PermissionGuard],
   exports: [EventService],
 })
 export class EventModule {}
