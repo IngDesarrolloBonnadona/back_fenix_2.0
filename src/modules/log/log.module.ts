20a78e8749575b763929025ee324808fdc8eda15
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-16 11:16:18 -0500

    se agrega permisos en log

--- src/modules/log/log.module.ts
+++ src/modules/log/log.module.ts
@@ -3,13 +3,13 @@ import { LogService } from './services/log.service';
 import { LogController } from './controllers/log.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './entities/log.entity';
+import { UserModule } from 'src/modules_bonnadonahub/user/user.module';
+import { PermissionGuard } from 'src/guards/permission.guard';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Log])
-  ],
+  imports: [TypeOrmModule.forFeature([Log]), UserModule],
   controllers: [LogController],
-  providers: [LogService],
-  exports: [LogService]
+  providers: [LogService, PermissionGuard],
+  exports: [LogService],
 })
 export class LogModule {}
