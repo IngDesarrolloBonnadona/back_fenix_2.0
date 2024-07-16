df02a64586065e9ab41a410c1fbf0a19d388ca80
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-16 15:07:35 -0500

    se agrega permisos en role permission

--- src/modules/role-permission/role-permission.module.ts
+++ src/modules/role-permission/role-permission.module.ts
@@ -3,11 +3,13 @@ import { RolePermissionService } from './services/role-permission.service';
 import { RolePermissionController } from './controllers/role-permission.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolePermission } from './entities/role-permission.entity';
+import { PermissionGuard } from 'src/guards/permission.guard';
+import { UserModule } from 'src/modules_bonnadonahub/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RolePermission])],
+  imports: [TypeOrmModule.forFeature([RolePermission]), UserModule],
   controllers: [RolePermissionController],
-  providers: [RolePermissionService],
+  providers: [RolePermissionService, PermissionGuard],
   exports: [RolePermissionService],
 })
 export class RolePermissionModule {}
