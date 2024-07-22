9bf0ba4c64a2c3e0c4c3ff69e9dfc21ce657e285
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-22 11:15:21 -0500

    se crea servicio y se mantiene la transaccion

--- src/modules/action-plan-case-report-validate/services/action-plan-case-report-validate.service.ts
+++ src/modules/action-plan-case-report-validate/services/action-plan-case-report-validate.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateActionPlanCaseReportValidateDto } from '../dto/create-action-plan-case-report-validate.dto';
 import { UpdateActionPlanCaseReportValidateDto } from '../dto/update-action-plan-case-report-validate.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryRunner, Repository } from 'typeorm';
+import { ActionPlanCaseReportValidate as ActionPlanCaseReportValidateEntity} from '../entities/action-plan-case-report-validate.entity';
 
 @Injectable()
 export class ActionPlanCaseReportValidateService {
-  create(createActionPlanCaseReportValidateDto: CreateActionPlanCaseReportValidateDto) {
-    return 'This action adds a new actionPlanCaseReportValidate';
+  constructor(
+    @InjectRepository(ActionPlanCaseReportValidateEntity)
+    private readonly actionPlanCaseReportValidateRepository: Repository<ActionPlanCaseReportValidateEntity>,
+  ) {}
+  async createActionPlanCaseReportValidateTransaction(
+    queryRunner: QueryRunner,
+    actionPlanCaseReportValidate: CreateActionPlanCaseReportValidateDto,
+  ) {
+    const actionPlanCRV = this.actionPlanCaseReportValidateRepository.create(
+      actionPlanCaseReportValidate
+    )
+
+    return await queryRunner.manager.save(actionPlanCRV);
   }
 
   findAll() {
@@ -16,7 +30,10 @@ export class ActionPlanCaseReportValidateService {
     return `This action returns a #${id} actionPlanCaseReportValidate`;
   }
 
-  update(id: number, updateActionPlanCaseReportValidateDto: UpdateActionPlanCaseReportValidateDto) {
+  update(
+    id: number,
+    updateActionPlanCaseReportValidateDto: UpdateActionPlanCaseReportValidateDto,
+  ) {
     return `This action updates a #${id} actionPlanCaseReportValidate`;
   }
 
