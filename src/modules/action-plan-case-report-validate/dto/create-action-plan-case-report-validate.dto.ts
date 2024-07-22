e240190ba8eef242698f167e6d7ab86d8637f350
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-22 11:15:36 -0500

    Se crea DTO

--- src/modules/action-plan-case-report-validate/dto/create-action-plan-case-report-validate.dto.ts
+++ src/modules/action-plan-case-report-validate/dto/create-action-plan-case-report-validate.dto.ts
@@ -1,1 +1,11 @@
-export class CreateActionPlanCaseReportValidateDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateActionPlanCaseReportValidateDto {
+    @IsString()
+    @IsNotEmpty()
+    plan_av_validatedcase_id_fk: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    plan_av_actionplan_id_fk: number;
+}
