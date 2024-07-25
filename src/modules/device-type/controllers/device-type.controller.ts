8a1eb877cc913e289c088399a4a7c5e6a4252b57
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-25 15:00:52 -0500

    se agrega los promises

--- src/modules/device-type/controllers/device-type.controller.ts
+++ src/modules/device-type/controllers/device-type.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   UseGuards,
+  HttpException,
 } from '@nestjs/common';
 import { DeviceTypeService } from '../services/device-type.service';
 import { CreateDeviceTypeDto } from '../dto/create-device-type.dto';
@@ -15,6 +16,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { PermissionGuard } from 'src/guards/permission.guard';
 import { Permission } from 'src/decorators/permission.decorator';
 import { permissions } from 'src/enums/permissions.enum';
+import { DeviceType } from '../entities/device-type.entity';
 
 @ApiTags('device-type')
 @Controller('device-type')
@@ -24,17 +26,19 @@ export class DeviceTypeController {
 
   @Post('/createDeviceType/:userIdPermission')
   @Permission(permissions.SUPER_ADMIN, permissions.PARAMETERIZER)
-  createDeviceType(@Body() createDeviceTypeDto: CreateDeviceTypeDto) {
+  createDeviceType(
+    @Body() createDeviceTypeDto: CreateDeviceTypeDto,
+  ): Promise<HttpException> {
     return this.deviceTypeService.createDeviceType(createDeviceTypeDto);
   }
 
   @Get('/listDeviceTypes/')
-  listDeviceTypes() {
+  listDeviceTypes(): Promise<DeviceType[]> {
     return this.deviceTypeService.findAllDeviceTypes();
   }
 
   @Get('/findDeviceType/:id')
-  findDeviceType(@Param('id') id: number) {
+  findDeviceType(@Param('id') id: number): Promise<DeviceType> {
     return this.deviceTypeService.findOneDeviceType(id);
   }
 
@@ -43,13 +47,13 @@ export class DeviceTypeController {
   updateDeviceType(
     @Param('id') id: number,
     @Body() updateDeviceTypeDto: UpdateDeviceTypeDto,
-  ) {
+  ): Promise<HttpException> {
     return this.deviceTypeService.updateDeviceType(id, updateDeviceTypeDto);
   }
 
   @Delete('/deleteDeviceType/:id/:userIdPermission')
   @Permission(permissions.SUPER_ADMIN, permissions.PARAMETERIZER)
-  deleteDeviceType(@Param('id') id: number) {
+  deleteDeviceType(@Param('id') id: number): Promise<HttpException> {
     return this.deviceTypeService.deleteDeviceType(id);
   }
 }
