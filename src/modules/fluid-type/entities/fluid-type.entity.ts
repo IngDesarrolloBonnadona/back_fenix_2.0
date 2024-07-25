fd8e7ff9cfee40bc6f1c8b4ea05c09d3281d0ba9
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-25 15:49:39 -0500

    se crea la tabla

---
+++ src/modules/fluid-type/entities/fluid-type.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'fenix_fluid_type' })
+export class FluidType {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  flu_t_name: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  flu_t_description: string;
+
+  @Column({ default: true })
+  flu_t_status: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
