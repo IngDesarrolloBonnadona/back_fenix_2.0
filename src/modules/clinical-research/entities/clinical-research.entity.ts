3214873d9eda1d64a96eb1f6c907d3cf0f06d9ba
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-26 16:26:39 -0500

    creacion de tabla en proceso

---
+++ src/modules/clinical-research/entities/clinical-research.entity.ts
@@ -0,0 +1,91 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity({ name: 'fenix_clinical_research' })
+export class ClinicalResearch {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: true })
+  res_c_instrument_id_fk: number;
+
+  @Column({ nullable: true })
+  res_c_failure: boolean;
+
+  @Column({ nullable: true })
+  res_c_damage: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_clinicalcontext: string;
+
+  @Column({ nullable: true })
+  res_c_devicetype_id_fk: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_otherdevicetype: string;
+
+  @Column({ nullable: true })
+  res_c_damagetype_id_fk: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_otherdamagetype: string;
+
+  @Column({ nullable: true })
+  res_c_fluidtype_id_fk: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_fluidname: string;
+
+  @Column({ nullable: true })
+  res_c_phlebitisgeneratingfluid: boolean;
+
+  @Column({ type: 'float', nullable: true })
+  res_c_fluidph: number;
+
+  @Column({ nullable: true })
+  res_c_adequateinfusiontime: boolean;
+
+  @Column({ type: 'float', nullable: true })
+  res_c_infusiontime: number;
+
+  @Column({ nullable: true })
+  res_c_adequatedilution: boolean;
+
+  @Column({ type: 'float', nullable: true })
+  res_c_fluiddilution: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_otherinfluencingfactors: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_otherfailedmeasures: string;
+
+  @Column({ nullable: true })
+  res_c_riskfactors_id_fk: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_otherriskfactors: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_venipuncturetechnique: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_additionalfindings: string;
+
+  @Column({ nullable: true })
+  res_c_carefailures: boolean;
+
+  @Column({ nullable: true })
+  res_c_safetybarriers_id_fk: number;
+
+  @Column({ nullable: true })
+  res_c_incorrectupdates: boolean;
+
+  @Column({ nullable: true })
+  res_c_unsafeactions: boolean;
+
+  @Column({ type: 'varchar', nullable: true })
+  res_c_conclusions: string;
+
+  @Column({ nullable: true })
+  res_c_casepreventable: boolean;
+}
