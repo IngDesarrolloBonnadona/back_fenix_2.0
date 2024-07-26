33b5e0f4672a2da25f8b3e6e431467361b0d6f12
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-26 08:30:04 -0500

    se crea logica de negocio CRUD

--- src/modules/risk-factor/controllers/risk-factor.controller.ts
+++ src/modules/risk-factor/controllers/risk-factor.controller.ts
@@ -6,40 +6,50 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RiskFactorService } from '../services/risk-factor.service';
 import { CreateRiskFactorDto } from '../dto/create-risk-factor.dto';
 import { UpdateRiskFactorDto } from '../dto/update-risk-factor.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { PermissionGuard } from 'src/guards/permission.guard';
+import { Permission } from 'src/decorators/permission.decorator';
+import { permissions } from 'src/enums/permissions.enum';
 
+@ApiTags('risk-factor')
 @Controller('risk-factor')
+@UseGuards(PermissionGuard)
 export class RiskFactorController {
   constructor(private readonly riskFactorService: RiskFactorService) {}
 
-  @Post()
-  create(@Body() createRiskFactorDto: CreateRiskFactorDto) {
+  @Post('/createRiskFactor/:userIdPermission')
+  @Permission(permissions.SUPER_ADMIN, permissions.PARAMETERIZER)
+  createRiskFactor(@Body() createRiskFactorDto: CreateRiskFactorDto) {
     return this.riskFactorService.createRiskFactor(createRiskFactorDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('/listRiskFactor/')
+  listRiskFactor() {
     return this.riskFactorService.findAllRiskFactors();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
+  @Get('/findRiskFactor/:id')
+  findRiskFactor(@Param('id') id: number) {
     return this.riskFactorService.findOneRiskFactor(id);
   }
 
-  @Patch(':id')
-  update(
+  @Patch('/updateRiskFactor/:id/:userIdPermission')
+  @Permission(permissions.SUPER_ADMIN, permissions.PARAMETERIZER)
+  updateRiskFactor(
     @Param('id') id: number,
     @Body() updateRiskFactorDto: UpdateRiskFactorDto,
   ) {
     return this.riskFactorService.updateRiskFactor(id, updateRiskFactorDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
+  @Delete('/deleteRiskFactor/:id/:userIdPermission')
+  @Permission(permissions.SUPER_ADMIN, permissions.PARAMETERIZER)
+  deleteRiskFactor(@Param('id') id: number) {
     return this.riskFactorService.deleteRiskFactor(id);
   }
 }
