15b9a65a6b620f5caa3ecf5af6db060c83cef34a
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-07-31 15:26:56 -0500

    se corrige dinamizando la generacion del consecutivo del caso

--- src/modules/case-report-original/utils/helpers/generate_filing_number.helper.ts
+++ src/modules/case-report-original/utils/helpers/generate_filing_number.helper.ts
@@ -5,16 +5,38 @@ export async function generateFilingNumber(
     caseReportOriginalRepository: Repository<CaseReportOriginal>
 ) {
     const lastReport = await caseReportOriginalRepository
+    // .createQueryBuilder('case_report_original')
+    // .select('case_report_original.ori_cr_filingnumber')
+    // .orderBy('case_report_original.ori_cr_filingnumber', 'DESC')
+    // .getOne();
+
+    // let newNumber: number;
+
+    // if (lastReport && lastReport.ori_cr_filingnumber) {
+    //     const lastNumber = parseInt(lastReport.ori_cr_filingnumber, 10);
+    //     newNumber = lastNumber + 1;
+    // } else {
+    //     newNumber = 1
+    // }
+
+    // return newNumber.toString().padStart(10, '0');
+
     .createQueryBuilder('case_report_original')
     .select('case_report_original.ori_cr_filingnumber')
-    .orderBy('case_report_original.ori_cr_filingnumber', 'DESC')
+    .orderBy('case_report_original.createdAt', 'DESC')
     .getOne();
+
+    let newNumber: number;
 
     if (lastReport && lastReport.ori_cr_filingnumber) {
         const lastNumber = parseInt(lastReport.ori_cr_filingnumber, 10);
-        const newNumber = lastNumber + 1;
-        return newNumber.toString().padStart(10, '0');
+        newNumber = lastNumber + 1;
     } else {
-        return '0000000001'
+        newNumber = 1
     }
+    
+    const lengthToPad = newNumber.toString().length + 1;
+    const filingNumber = newNumber.toString().padStart(lengthToPad, '0');
+
+    return filingNumber;
 }
