afd0f42211334af2208c970927302aa2d7d9f4e2
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-08-01 16:28:06 -0500

    se corrige los HttpStatus y se agrega nuevas Exepciones para validar parametros o headers

--- src/modules/unit/dto/create-unit.dto.ts
+++ src/modules/unit/dto/create-unit.dto.ts
@@ -1,10 +1,6 @@
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateUnitDto {
-  // @IsNumber()
-  // @IsNotEmpty()
-  // unit_service_id_fk: number;
-
   @IsNotEmpty()
   @IsString()
   unit_name: string;
