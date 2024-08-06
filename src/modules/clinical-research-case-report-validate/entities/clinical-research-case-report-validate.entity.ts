486b8f7f1fb97d124527ed8966ce7566764b0856
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-08-06 11:08:12 -0500

    se crea la entidad

--- src/modules/clinical-research-case-report-validate/entities/clinical-research-case-report-validate.entity.ts
+++ src/modules/clinical-research-case-report-validate/entities/clinical-research-case-report-validate.entity.ts
@@ -1,1 +1,25 @@
-export class ClinicalResearchCaseReportValidate {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'fenix_clinical_research_case_report_validate' })
+export class ClinicalResearchCaseReportValidate {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  res_crv_devicetype_id_fk: number;
+
+  @Column({ type: 'uuid' })
+  res_crv_validatedcase_id_fk: string;
+
+  @Column({ default: true })
+  res_crv_status: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
